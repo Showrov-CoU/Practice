3b58bdfe6e81b272f1681a52a10f13a4fbff30cd
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-05-05 18:40:30 +0600

    again git practice

---
+++ fs.js
@@ -0,0 +1,6 @@
+var myName = "Md.Ashikur Rahman Showrov";
+console.log(myName.length);
+
+// ...........JS-1-b............
+let myArray = [3,4,9,"a","ant","nure",[4,5,9]];
+console.log(myArray.length)
